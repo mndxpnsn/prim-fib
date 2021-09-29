e0dcddd1eae9afb5155470d6279263e34b8e5c4f
derek <harrison.derek.w@gmail.com>
2021-09-29 23:26:52 +0200

    revise complexity estimation

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,8 @@ int main(int argc, char* argv[])
 {
     //Declarations
     int s = 2; //Start vertex. The minimum index for vertices is 1
-    int n = 20499; //Number of vertices
-    int num_edges = 20000; //Number of edges
+    int n = 2499; //Number of vertices
+    int num_edges = 20499; //Number of edges
 
     //Create edges
     std::vector< edge > edges;
@@ -44,11 +44,11 @@ int main(int argc, char* argv[])
     mst_props min_span_props = mst(n, edges, s);
 
     //Print results
-    float tot_num_ops_est = 5 * n + 3 * num_edges + 5 * n * log(n) / log(2);
-    print_mst(n, min_span_props.node_arr);
+    float tot_num_ops_est = 6 * n + 3 * num_edges + 5 * n * log(n) / log(2);
+//    print_mst(n, min_span_props.node_arr);
     std::cout << "size of minimum spanning tree: " << min_span_props.mst_weight << std::endl;
     std::cout << "total number of operations measured: " << tot_num_ops << std::endl;
-    std::cout << "estimated number of operations 5V + 3E + 5VlgV: " << tot_num_ops_est << std::endl;
+    std::cout << "estimated number of operations 6V + 3E + 5VlgV: " << tot_num_ops_est << std::endl;
     std::cout << "ratio complexities: " << (tot_num_ops / tot_num_ops_est) << std::endl;
     std::cout << "done" << std::endl;
 
